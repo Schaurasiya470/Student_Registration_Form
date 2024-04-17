cecdf8f66b5a0d4668cc6b1e92a7443abf8032a8
sachin@git <schaurasiya470@gmail.com>
2024-04-17 12:34:53 +0530

    database connectivity test

--- backend_src/database.php
+++ backend_src/database.php
@@ -1,12 +1,12 @@
 <?php
 
 // $servername = "115.243.44.218";
-$servername = "srv-dev.dayalinfosystems.com";
+$servername = "http://srv-dev.dayalinfosystems.com";
 $username = "sachinc_bit";
-$password = "Sc#9525&";
+$password = "Sc#9525&"; 
 $dbname = "db_sachinc_bit";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli_connect($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -15,3 +15,5 @@ echo "MySQL DB Connected successfully!";
 
 $conn->close();
 ?>
+
+
