cb49d66f2fdf185079e5e5d858838507c77e8cc3
sachin@git <schaurasiya470@gmail.com>
2024-04-20 03:35:22 +0530

    GET_API file updated

--- backend_src/GET_API.php
+++ backend_src/GET_API.php
@@ -1,13 +1,25 @@
 <?php
     header('content-Type: application/json');
-    header('acess-Control-Allow-Origin: *');
+    // header('content-Type: image/jpeg');
+    header('access-Control-Allow-Origin: *');
 
     include "database.php";
 
-    $sql  = "SELECT id,first_name,last_name,email_id FROM tbl_student_registration";
+    $sql  = "SELECT 
+    id ,first_name ,last_name ,email_id ,aadhar_no,date_of_birth,mobile ,gender,country,state,city,street,landmark,pincode,username
+    ,password  ,hobbies ,created_date
+    FROM tbl_student_registration";
     $result = mysqli_query($conn,$sql) or die("QUERY not Compiled") ;
 
     if(mysqli_num_rows($result)>0){
+        // foreach($result as $row){
+        //     echo $row['id'];
+        //     echo $row['first_name'];  echo $row['date_of_birth']; 
+        //     echo $row['created_date'];  echo $row['mobile'];
+        //     <img src = "data:image/jpeg" echo $row["image_blb"]' height="100px" width=100px/>' ;
+        //     echo "\n";
+
+        // }
         $output = mysqli_fetch_all($result,MYSQLI_ASSOC);
         echo json_encode($output);
     }else{
